3f49c766489b8777c04f3f477bf2600c5f92f85b
assyl.dev <asyl.batyrkhan@jihc.edu.kz>
2025-05-09 16:36:10 +0500

    create base ui

---
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_icon_class/font_awesome_icon_class.dart';
+import 'package:iconsax_plus/iconsax_plus.dart';
+import 'package:weather_app2/features/geolocation/presentation/screens/select_geolocation.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  int tabIndex = 0;
+  bool isVisible = false;
+  late TabController tabController;
+
+  List<Widget> screens = [
+    Center(child: Text("Weather Page")),
+    Center(child: Text("Forecast Page")),
+    Center(child: Text("Weather Page")),
+    Center(child: Text("Forecast Page")),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setupTabController();
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  void setupTabController() {
+    tabController = TabController(
+      initialIndex: tabIndex,
+      length: screens.length,
+      vsync: this,
+    );
+
+    tabController.animation?.addListener(() {
+      int value = (tabController.animation!.value).round();
+      if (value != tabIndex) setState(() => tabIndex = value);
+    });
+
+    tabController.addListener(() {
+      setState(() {
+        tabIndex = tabController.index;
+      });
+    });
+  }
+
+  void changeTabIndex(int index) {
+    setState(() {
+      tabIndex = index;
+    });
+    tabController.animateTo(tabIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: switch (tabIndex) {
+          0 => IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SelectGeolocation(isStart: false),
+                ),
+              );
+            },
+            icon: Icon(IconsaxPlusLinear.global_search),
+          ),
+          int() => null,
+        },
+        title: switch (tabIndex) {
+          0 => isVisible ? TextField() : Text("Taraz"),
+          int() => null,
+        },
+        actions: switch (tabIndex) {
+          0 => [
+            IconButton(
+              onPressed: () {
+                if (isVisible) {
+                  isVisible = false;
+                } else {
+                  isVisible = true;
+                }
+                setState(() {});
+              },
+              icon: Icon(IconsaxPlusLinear.search_normal_1),
+            ),
+          ],
+          int() => null,
+        },
+      ),
+      body: TabBarView(controller: tabController, children: screens),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: tabIndex,
+        onDestinationSelected: (int index) => changeTabIndex(index),
+        destinations: [
+          NavigationDestination(
+            icon: Icon(IconsaxPlusLinear.cloud_sunny),
+            selectedIcon: Icon(IconsaxPlusBold.cloud_sunny),
+            label: "Weather",
+          ),
+          NavigationDestination(
+            icon: Icon(IconsaxPlusLinear.building),
+            selectedIcon: Icon(IconsaxPlusBold.building),
+            label: "Weather",
+          ),
+          NavigationDestination(
+            icon: Icon(IconsaxPlusLinear.map),
+            selectedIcon: Icon(IconsaxPlusBold.map),
+            label: "Weather",
+          ),
+          NavigationDestination(
+            icon: Icon(IconsaxPlusLinear.category),
+            selectedIcon: Icon(IconsaxPlusBold.category),
+            label: "Weather",
+          ),
+        ],
+      ),
+    );
+  }
+}
