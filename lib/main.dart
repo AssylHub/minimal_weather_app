509773e0530f7002543f66aae13e6b930f8521f8
assyl.dev <asyl.batyrkhan@jihc.edu.kz>
2025-05-11 21:06:46 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app2/features/geolocation/data/datasources/geolocation_data_source_impl.dart';
 import 'package:weather_app2/features/geolocation/di/geolocation_di.dart';
-import 'package:weather_app2/features/geolocation/presentation/screens/select_geolocation.dart';
 import 'package:weather_app2/features/home/presentation/screens/home_screen.dart';
 
 void main() async {
-  // final geolocationDataSource = GeolocationDataSourceImpl();
   WidgetsFlutterBinding.ensureInitialized();
   init();
-  // Test with a known city
-  // final city = 'Biskek';
-  // try {
-  //   final location = await geolocationDataSource.getCurrentLocation();
-  //   print('City: ${location.city}');
-  //   print('Latitude: ${location.latitude}');
-  //   print('Longitude: ${location.longitude}');
-  //   print('District: ${location.district}');
-  // } catch (e) {
-  //   print('Error: $e');
-  // }
-
   runApp(const MyApp());
 }
 
